78c4e5377cef91c6b7b59c412566abbcd567e44a
Yauheni Slizh <yauheni.slizh@tyteishi.com>
2026-02-02 21:20:43 +0100

    Remove description from plugin properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,12 +35,6 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         name = "Typing Training"
-        description =
-            """
-            Improved stability and usability.
-            - Added ability to drag typing dialog to any position on the screen.
-            - Improved stability.
-            """.trimIndent()
         changeNotes =
             """
             - Fixed incorrect representation of the time on the final statistic dialog.
